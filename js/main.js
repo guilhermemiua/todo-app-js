02b3286342e492a4429bd1ee705b154aea6ddaf2
guilhermemiua <guilhermemiua@gmail.com>
2018-11-08 12:17:14 -0200

    Checkbox, comments, image onload added

--- js/main.js
+++ js/main.js
@@ -9,14 +9,29 @@ submitTask.addEventListener('click', (e) => {
     let listItem = document.createElement('li');
     let value = document.createTextNode(inputTask.value);
     let removeButton = document.createElement('img');
+    let checkbox = document.createElement('input');
 
-    div.classList.add("listItem");
+    // Adding list item to a div
+    div.classList.add('listItem');
 
-    removeButton.src = "../img/close-outline.svg";
+    // Remove button attributes
     removeButton.classList.add("remove");
 
+    // Onload image
+    let downloadingImage = new Image();
+    downloadingImage.src = "../img/close-outline.svg";
+    downloadingImage.onload = () => {
+      removeButton.src = downloadingImage.src;
+    };
+
+    // checkbox
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+
+    // Adding list item
     listItem.classList.add('listItem');
     listItem.appendChild(value);
+    div.appendChild(checkbox);
     div.appendChild(listItem);
     div.appendChild(removeButton);
     taskList.appendChild(div);
@@ -24,6 +39,12 @@ submitTask.addEventListener('click', (e) => {
     // remove by clicking on it
     removeButton.addEventListener('click', (e) => {
       removeButton.parentElement.remove();
+    });
+
+    // task completed
+    checkbox.addEventListener('click', (e) => {
+      checkbox.parentElement.remove();
+      console.log(checkbox.parentElement.children[1].innerText);
     });
   }
 });
